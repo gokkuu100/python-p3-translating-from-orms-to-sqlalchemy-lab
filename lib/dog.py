529a7081fe73771dad26d45f5c36cd9c31401803
goku <wkurts247@gmail.com>
2023-09-09 09:57:47 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,25 @@
-from models import Dog
+from models import Dog  # Import your Dog model from models.py
+from sqlalchemy.orm import Session, base
 
 def create_table(base):
-    pass
+    base.metadata.create_all()
 
-def save(session, dog):
-    pass
+def save(session: Session, dog: Dog):
+    session.add(dog)
+    session.commit()
 
-def get_all(session):
-    pass
+def get_all(session: Session):
+    return session.query(Dog).all()
 
-def find_by_name(session, name):
-    pass
+def find_by_name(session: Session, name: str):
+    return session.query(Dog).filter_by(name=name).first()
 
-def find_by_id(session, id):
-    pass
+def find_by_id(session: Session, id: int):
+    return session.query(Dog).filter_by(id=id).first()
 
-def find_by_name_and_breed(session, name, breed):
-    pass
+def find_by_name_and_breed(session: Session, name: str, breed: str):
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
-def update_breed(session, dog, breed):
-    pass
+def update_breed(session: Session, dog: Dog, new_breed: str):
+    dog.breed = new_breed
+    session.commit()
